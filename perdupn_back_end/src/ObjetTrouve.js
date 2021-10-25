79f1a21707f4ab49524228aedacf6bdb8ceb6301
meissonnieraphael <meissonnier.raphael@gmail.com>
2021-10-25 22:43:33 +0200

    Update Interface Objet

--- perdupn_back_end/src/ObjetTrouve.js
+++ perdupn_back_end/src/ObjetTrouve.js
@@ -4,9 +4,9 @@ const Calculateur = require('./Calculateur');
 
 class ObjetTrouve extends Objet
 {
-    constructor(categorie, localisation)
+    constructor(categorie, localisation, description, intitule, date)
     {
-        super(categorie, localisation);
+        super(categorie, localisation, description, intitule, date);
         this.calculateur = new Calculateur();
 
     }
