474807cfc30f2bce6d5c41b41afc2c7e1d6bb563
meissonnieraphael <meissonnier.raphael@gmail.com>
2021-12-01 21:44:56 +0100

    Update structure of back

--- perdupn_back_end/server/src/Calculateur.js
+++ perdupn_back_end/server/src/Calculateur.js
@@ -9,58 +9,33 @@ class Calculateur
 
     getDistanceLocalisationPrecise(localisationUser, localisationObjet)
     {
-            if(localisationObjet instanceof Localisation && localisationUser instanceof Localisation)
-            {
-                //A MOCKER ?
-                function toRad(x){
-                    return x * Math.PI/180;
-                }
-
-                //On récupère la position et les coordonnées du user
-                let positionUser = localisationUser.getPosition();
-                let longitudeUser = positionUser.getLongitude();
-                let latitudeUser = positionUser.getLatitude();
-
-                //On récupère la position et les coordonnées de l'objet
-                let positionObjet = localisationObjet.getPosition();
-                let longitudeObjet = positionObjet.getLongitude();
-                let latitudeObjet = positionObjet.getLatitude();
-
-                var rayon= 10; //en Km
-
-                var latitudeDis= latitudeObjet-latitudeUser;
-                var dLat= toRad(latitudeDis);
-                var longitudeDis= longitudeObjet-longitudeUser;
-                var dLong= toRad(longitudeDis);
-
-                var a= Math.sin(dLat/2)* Math.sin(dLat/2) +
-                    Math.cos(toRad(latitudeUser)) * Math.cos(toRad(latitudeObjet)) *
-                    Math.sin(dLong/2) * Math.sin(dLong/2);
-
-                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-                var d = rayon * c;
-
-                return d;
-            }
+        if(localisationObjet instanceof Localisation && localisationUser instanceof Localisation)
+        {
+            var positionUser = localisationUser.getPosition();
+            var positionObjet = localisationObjet.getPosition();
+            var user = { lat: positionUser.getLatitude(), lng: positionUser.getLongitude() }    ;
+            var objet = { lat: positionObjet.getLatitude(), lng: positionObjet.getLongitude() }   ;
+            return (haversine(user, objet))/1000;
+        }
     }
 
     getDistanceLocalisationFloue(localisationUser, localisationObjet)
     {
         if(localisationObjet instanceof Localisation && localisationUser instanceof Localisation)
         {
-            //On récupère la position et les coordonnées du user
-            let positionUser = localisationUser.getPosition();
-            let longitudeUser =  positionUser.getLongitude();
-            let latitudeUser =  positionUser.getLatitude();
-
-            //On récupère la position, les coordonnées et le rayon de l'objet
-            let positionObjet = localisationObjet.getPosition();
-            let longitudeObjet =  positionObjet.getLongitude();
-            let latitudeObjet =  positionObjet.getLatitude();
+            //On récupère le rayon de l'objet
             let rayon = localisationObjet.getRayon();
 
-            //On calcule la distance avec la formule |sqrt((x1 - h)² + (y1 - k)²) - r|
-            let distance = Math.sqrt((Math.pow((longitudeUser - longitudeObjet), 2)) + (Math.pow((latitudeUser - latitudeObjet), 2))) - rayon;
+            //On récupère la longitude et latitude du user
+            var positionUser = localisationUser.getPosition();
+            var positionObjet = localisationObjet.getPosition();
+            var user = { lat: positionUser.getLatitude(), lng: positionUser.getLongitude() };
+
+            //On récupère la longitude et latitude de l'objet
+            var objet = { lat: positionObjet.getLatitude(), lng: positionObjet.getLongitude() };
+
+            //On calcule la distance entre l'objet et l'user
+            var distance = ((haversine(user, objet))/1000)-rayon;
             if(distance < 0)
             {
                 distance = distance * (-1);
