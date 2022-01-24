92d27ba6f14ba03968ab162d394510710fed1738
meissonnieraphael <meissonnier.raphael@gmail.com>
2022-01-24 16:22:40 +0100

    update query

--- perdupn_back_end/server/config/database.js
+++ perdupn_back_end/server/config/database.js
@@ -2,7 +2,7 @@
 const Sequelize = require("sequelize");
  
 // create connection
-const db = new Sequelize('foundem2', 'root', 'Imane213', {
+const db = new Sequelize('foundem2', 'rootDB', 'rootDB', {
     host: '127.0.0.1',
     dialect: 'mysql'
 });
